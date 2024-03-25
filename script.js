c5d26db776656de475bfa279c73bac608ca1cf3b
Kamal Paudel <techitus07@gmail.com>
2024-03-25 18:41:14 +0545

    completed

--- script.js
+++ script.js
@@ -20,6 +20,51 @@ todoList.textContent = ""
 tasks.forEach((task)=>{
     var listItem = document.createElement('li')
     listItem.textContent = task.text;
+    listItem.className = task.completed? "completed" : "";
+    listItem.onclick = function(){
+        toggleCompleted(task)
+    }
 todoList.appendChild(listItem)
 })
+updateAggregrate()
+
+}
+function toggleCompleted(task){
+    task.completed =!task.completed;
+    updateTodoList()
+}
+function updateAggregrate(){
+    var totalTasks = document.getElementById("totalTasks")
+    var completedTasks = document.getElementById("completedTasks")
+    var total = tasks.length
+  var completed =   tasks.reduce((acc,task)=>{
+return task.completed ? acc + 1 : acc
+    },0)
+    totalTasks.textContent = total
+    completedTasks.textContent = completed
+}
+    function filterTasks(){
+        var searchInput = document.getElementById('searchInput')
+        var searchValue = searchInput.value.toLowerCase()
+       var filteredTasks=  tasks.filter((task)=>{
+            return task.text.toLowerCase().includes(searchValue)
+        })
+        updateTodoListWithFilterTasks(filteredTasks)
+
+    }
+    
+
+function updateTodoListWithFilterTasks(filteredTasks){
+    const todoList = document.getElementById('todoList')
+    todoList.textContent = ""
+    filteredTasks.forEach((task)=>{
+        var listItem = document.createElement('li')
+        listItem.textContent = task.text;
+        listItem.className = task.completed? "completed" : "";
+        listItem.onclick = function(){
+            toggleCompleted(task)
+        }
+    todoList.appendChild(listItem)
+    })
+    updateAggregrate()
 }
